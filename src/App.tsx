9675c191698ceee9ce0fc2ed24441b0d04f04fdd
belloshe <bellabello199@hotmail.com>
2023-10-30 22:57:03 +0100

    tagit bort onödig kod

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+
 import BabyNames from './components/BabyNames';
 
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <BabyNames />
+      <BabyNames/>
     </div>
   );
 }
